58cef43678c8790d60a99c9e5a69e806f3ef419b
Alexander <Alexander@LENOVO-IDEAPAD>
2022-04-26 13:39:49 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinUIEx;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace LAB_8 {
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : WindowEx {
+        private ObservableCollection<Shape> shapes = new();
+        private ObservableCollection<string> shapesDesc = new();
+
+        public MainWindow() {
+            InitializeComponent();
+
+            ShapeListBox.ItemsSource = shapes;
+
+            shapes.CollectionChanged += Shapes_CollectionChanged;
+        }
+
+        private void Shapes_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) {
+            // Check if we need to enable the clear button
+            ClearButton.IsEnabled = !(shapes.Count == 0);
+
+            // Update descriptions?
+
+        }
+
+        private void AddShapeSquare_Click(object sender, RoutedEventArgs e) {
+            Random R = new();
+            Shape shape = new Square(Math.Round(R.NextDouble() * R.Next(10, 30), 3));
+
+            if (ShapeListBox.SelectedIndex == -1)
+                // Add to the end of the list
+                shapes.Add(shape);
+            else
+                shapes.Insert(ShapeListBox.SelectedIndex, shape);
+        }
+
+        private void AddShapeTriangle_Click(object sender, RoutedEventArgs e) {
+            Random R = new();
+            Shape shape = new Triangle(Math.Round(R.NextDouble() * R.Next(10, 30), 3));
+
+            if (ShapeListBox.SelectedIndex == -1)
+                // Add to the end of the list
+                shapes.Add(shape);
+            else
+                // Or insert on the selected position
+                shapes.Insert(ShapeListBox.SelectedIndex, shape);
+        }
+
+        private void DeleteSelectedButton_Click(object sender, RoutedEventArgs e) {
+            shapes.RemoveAt(ShapeListBox.SelectedIndex);
+            // Make sure to clear selection so it won't raise index error
+            ShapeListBox.SelectedIndex = -1;
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e) {
+            shapes.Clear();
+        }
+
+        private void ShapeListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            DeleteSelectedButton.IsEnabled = !(ShapeListBox.SelectedIndex == -1);
+        }
+
+        private void ExitApplication(object sender, RoutedEventArgs e) {
+            Environment.Exit(0);
+        }
+
+        private bool eight = true;
+        private void Hyperlink_Click(Hyperlink sender, HyperlinkClickEventArgs args) {
+            if (eight) {
+                sender.Inlines.Clear();
+                sender.Inlines.Add(new Run() { Text = "🎉" });
+            }
+            else {
+                sender.Inlines.Clear();
+                sender.Inlines.Add(new Run() { Text = "№8" });
+            }
+
+            eight = !eight;
+        }
+    }
+}
---
+++ Shapes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_8 {
+    public abstract class Shape : object {
+
+        public double Side { get; set; }
+        public abstract char TextIcon { get; }
+
+        public abstract double GetArea();
+
+        // Generic ToString function.          Avoid using class name
+        public new virtual string ToString() => $"{GetType().Name} [{TextIcon}] (Side: {Side}, Area: { Math.Round(GetArea(), 3)})";
+        
+        // Property for binding.
+        // For some reason, binding ignores overwritten ToString method and only displays class name.
+        public string StringRepr { get => ToString(); }
+    }
+
+    public class Triangle : Shape {
+        // Required properties
+        public override char TextIcon { get; } = '▲';
+
+        // Additional properties
+        // Better only recalculate result only when neccessary, but whatever ¯\_(ツ)_/¯
+        public double Altitude { get => Side * Math.Sqrt(3.0) / 2; }
+
+        // Constructor
+        public Triangle(double a) {
+            Side = a;
+        }
+
+        public override double GetArea() => (Altitude * Side) / 2;
+
+        public override string ToString() => $"{GetType().Name} [{TextIcon}] (Side: {Side}, Altitude: {  Math.Round(Altitude, 3) }, Area: { Math.Round(GetArea(), 3) })";
+    }
+
+    public class Square : Shape {
+        // Required properties
+        public override char TextIcon { get; } = '⬛';
+
+        // Constructor
+        public Square(double a) {
+            Side = a;
+        }
+
+        public override double GetArea() => Side * Side;
+    }
+}
